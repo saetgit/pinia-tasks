9a74a5f65609a076390e90e9edbdf8f25218d253
saetgit <etemadian.sara.1998@gmail.com>
2024-06-01 14:14:33 +0330

    create TaskDetails component and fetch data in stores

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
-<script setup>
-</script>
-
 <template>
-
-
   <main>
     <header>
       <img alt="Pinia logo" src="./assets/Pinialogo.png" />
-      <h1>Pinia Tasks</h1>
+      <h1>Pinia Task</h1>
     </header>
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetail :task="task" />
+      </div>
+    </div>
   </main>
 </template>
+
+<script setup>
+import { useTaskStore } from './stores/TaskStore';
+import TaskDetail from './components/TaskDetails.vue';
+
+const taskStore = useTaskStore();
+</script>
 
 <style scoped></style>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="task">
+        <p>{{ task.title }}</p>
+        <div class="icons">
+            <i class="material-icons">delete</i>
+            <i class="material-icons">favorite</i>
+        </div>
+    </div>
+</template>
+<script setup>
+defineProps({
+    task: {
+        type: Object,
+        required: true
+    }
+});
+</script>
+
+
+
+<style scoped></style>
