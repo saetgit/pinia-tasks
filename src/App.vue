66e385f5b0494cd8c7fa1babe2e9d0f6184845dc
saetgit <etemadian.sara.1998@gmail.com>
2024-06-01 17:50:58 +0330

    create reset state

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,24 @@
       <button @click="filter = 'all'">All Tasks</button>
       <button @click="filter = 'favs'">Fav tasks</button>
     </nav>
-    <div class="loading" v-if="taskStore.loading">Loading Task</div>
+    <div class="loading" v-if="loading">Loading Task</div>
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} tasks left to do.</p>
-      <div v-for="task in taskStore.tasks" :key="task.id">
+      <p>You have {{ totalCount }} tasks left to do.</p>
+      <div v-for="task in tasks" :key="task.id">
         <TaskDetail :task="task" />
       </div>
     </div>
 
     <div class="task-list" v-if="filter === 'favs'">
-      <p>You have {{ taskStore.favCount }} tasks in your favs list.</p>
-      <div v-for=" task in taskStore.fav" :key="task.id">
+      <p>You have {{ favCount }} tasks in your favs list.</p>
+      <div v-for=" task in fav" :key="task.id">
         <TaskDetail :task="task" />
       </div>
     </div>
+    <button @click="taskStore.$reset">rest state</button>
+
   </main>
+  
 </template>
 
 <script setup>
@@ -33,8 +36,10 @@ import { ref } from 'vue'
 import { useTaskStore } from './stores/TaskStore';
 import TaskDetail from './components/TaskDetails.vue';
 import TaskForm from './components/TaskForm.vue';
-
+import { storeToRefs } from 'pinia';
 const taskStore = useTaskStore();
+const { tasks, loading, fav, totalCount, favCount } = storeToRefs(taskStore)
+
 taskStore.getTasks()
 const filter = ref('all')
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -19,26 +19,57 @@ export const useTaskStore = defineStore('taskStore', {
         }
     },
     actions: {
-        async getTasks(){
-            this.loading=true
-            const res=await fetch('http://localhost:3000/tasks')
-            const data=await res.json()
-            this.tasks=data
-            this.loading=false
+        async getTasks() {
+            this.loading = true
+            const res = await fetch('http://localhost:3000/tasks')
+            const data = await res.json()
+            this.tasks = data
+            this.loading = false
         },
 
-        addTask(task) {
+        async addTask(task) {
             this.tasks.push(task)
+            const res = await fetch('http://localhost:3000/tasks', {
+                method: 'POST',
+                body: JSON.stringify(task),
+                headers: { 'Content-Type': 'application/json' }
+            })
+            if (res.error) {
+                console.log(res.error);
+            }
         },
-        deleteTask(id) {
+        async deleteTask(id) {
             this.tasks = this.tasks.filter(t => {
                 return t.id !== id
             })
+            const res = await fetch('http://localhost:3000/tasks/' + id, {
+                method: 'DELETE',
+            })
+            if (res.error) {
+                console.log(res.error);
+            }
         },
-        toggleFav(id) {
-            const task = this.tasks.find(t => t.id === id)
-            task.isFav = !task.isFav
-        }
+        async toggleFav(id) {
+            const task = this.tasks.find(t => t.id === id);
+            task.isFav = !task.isFav;
+          
+            try {
+              const res = await fetch('http://localhost:3000/tasks/' + id, {
+                method: 'PATCH',
+                body: JSON.stringify({ isFav: task.isFav }),
+                headers: { 'Content-Type': 'application/json' }
+              });
+          
+              if (!res.ok) {
+                const errorData = await res.json();
+                console.log('Error:', errorData);
+              } else {
+                console.log('Task updated successfully');
+              }
+            } catch (error) {
+              console.error('Fetch error:', error);
+            }
+          }
     }
 })
 
